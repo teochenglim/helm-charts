d2b65ec6bfc63397799a452f6ccf1578eec414b6
Teo Cheng Lim <teochenglim@gmail.com>
2022-03-02 09:33:30 +0800

    fixed charts var

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,5 +1,3 @@
 helm plugin install https://github.com/instrumenta/helm-kubeval
-pwd
-ls -lR
 charts=$(ls -d charts/*)
-for c in charts; do helm kubeval $c; done
+for c in $charts; do helm kubeval $c; done
